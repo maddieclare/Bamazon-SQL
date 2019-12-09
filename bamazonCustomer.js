1b6aba3548edc778ddb47dcd52dfd221a747df49
Maddie <madeleinecgribble@gmail.com>
2019-12-09 15:24:41 +0800

    Inquirer module working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql");
+const inquirer = require("inquirer");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -19,12 +20,32 @@ function Item(id, name, price) {
   };
 }
 
-connection.query("SELECT * FROM products", function(err, results) {
-  if (err) throw error;
+getListOfItems();
 
-  console.log("\nCurrent items for sale:\n")
-  for (let result of results) {
-    let item = new Item(result.item_id, result.product_name, result.price);
-    item.printItemDetails();
-  }
-});
+function getListOfItems() {
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw error;
+    // console.log(userPromptCall);
+
+    console.log("\nCurrent items for sale:\n");
+    for (let result of results) {
+      let item = new Item(result.item_id, result.product_name, result.price);
+      item.printItemDetails();
+    }
+
+    promptUserForId();
+  });
+}
+
+function promptUserForId() {
+  inquirer
+    .prompt([
+      {
+        name: "get_id",
+        message: "Please enter the ID of the item you would like to purchase:"
+      }
+    ])
+    .then(function(response) {
+      console.log(response);
+    });
+}
